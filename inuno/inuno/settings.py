eb0612c986ff1805b260daacd0b3d55a56109373
Abasiomofon Udoh <abasiomofonudoh@gmail.com>
2024-12-17 11:51:25 +0100

    update settings.py

--- inuno/inuno/settings.py
+++ inuno/inuno/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["inuno.onrender.com"]
+ALLOWED_HOSTS = ["inuno-ai.onrender.com", "127.0.0.1"]
 
 
 # Application definition
